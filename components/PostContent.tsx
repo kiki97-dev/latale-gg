32811d1ce9349d473f9d43caa2f84df25be20964
lsc <dltmd1502@naver.com>
2025-03-04 18:32:13 +0900

    feat: 수정 모달창 컴포넌트 분리

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -28,6 +28,7 @@ import { DialogFooter } from "@material-tailwind/react";
 import { Textarea } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
 import Link from "next/link";
+import UpdateContentModal from "./UpdateContentModal";
 
 interface PostContentProps {
 	post: Post;
@@ -383,71 +384,7 @@ export default function PostContent({ post, detail = false }: PostContentProps) 
 					</div>
 				</article>
 			</Link>
-			<Dialog
-				open={open}
-				size="xs"
-				className="bg-[#17222D] border border-[#384D63] z-[9999]"
-				dismiss={{ outsidePress: false, escapeKey: false }}
-				onClick={(e) => {
-					e.preventDefault();
-					e.stopPropagation();
-				}}
-			>
-				<div className="flex justify-end px-4 py-3" onClick={(e) => e.stopPropagation()}>
-					<IconButton
-						color="white"
-						size="sm"
-						variant="text"
-						onClick={(e) => {
-							// 수정 로직 구현
-							closeModal(e);
-						}}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							strokeWidth={2}
-							className="h-5 w-5"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M6 18L18 6M6 6l12 12"
-							/>
-						</svg>
-					</IconButton>
-				</div>
-				<DialogBody>
-					<div className="grid gap-6">
-						<Textarea color="white" label="Message" />
-					</div>
-				</DialogBody>
-				<DialogFooter className="space-x-2">
-					<Button
-						variant="text"
-						color="white"
-						onClick={(e) => {
-							// 수정 로직 구현
-							closeModal(e);
-						}}
-					>
-						취소하기
-					</Button>
-					<Button
-						className="bg-[#15F5BA] text-[#261E5A]"
-						variant="gradient"
-						color="white"
-						onClick={(e) => {
-							// 수정 로직 구현
-							closeModal(e);
-						}}
-					>
-						등록하기
-					</Button>
-				</DialogFooter>
-			</Dialog>
+			<UpdateContentModal closeModal={closeModal} open={open} />
 		</>
 	);
 }
